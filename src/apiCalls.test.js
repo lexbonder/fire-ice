b530ad6216da9fa699c48dd8faa0b31dd24baa2c
Alex Bonder <lexbonder@gmail.com>
2018-02-22 13:01:00 -0700

    Bang head against wall trying to get this freakin async test to pass...

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -1,15 +1,25 @@
 import { getHouseData } from './apiCalls';
+import mockData from './mockData';
 
-describe('apiCalls', () => {
+describe('getHouseData', () => {
   beforeEach(() => {
     window.fetch = jest.fn().mockImplementation(() => {
       let mockFetch = new Promise((resolve, reject) => {
         resolve({
           ok: true,
           status: 200,
-          json: () => ({})
+          json: () => ({mockData})
         })
       })
+      return mockFetch;
     })
+  })
+
+  it('should retrieve house data', async () => {
+    expect(await getHouseData()).toEqual({mockData})
+  })
+
+  it('should return an error when the fetch fails', () => {
+    
   })
 })
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { App } from './App';
+import { houses } from '../../mockData';
 
 describe('App', () => {
-  // let wrapper;
-  // let mockAddHousesToStore;
+  let wrapper;
+  let mockAddHousesToStore;
+  let mockHouses;
 
-  // beforeEach(() => {
-  //   mockFetch = new Promise ((resolve, reject) => )
-  //   mockAddHousesToStore = jest.fn()
-  //   wrapper = shallow(<App addHousesToStore={mockAddHousesToStore} />)
-  // })
+  window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+    json: () => Promise.resolve({
+      houses
+    })
+  }))
 
-  // it('should match the snapshot', () => {
-  //   expect(wrapper).toMatchSnapshot()
-  // })
+  beforeEach(() => {
+    mockHouses = houses;
+    mockAddHousesToStore = jest.fn()
+    wrapper = shallow(<App addHousesToStore={mockAddHousesToStore} />)
+  })
+  
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  describe('componentDidMount', () => {
+    it('should call addHousesToStore with a houses object', async () => {
+      wrapper.instance().mockAddHousesToStore = jest.fn()
+      wrapper.instance().getHouseData = jest.fn().mockImplementation(()=> mockHouses)
+      const houses = await wrapper.instance().getHouseData()
+      expect(wrapper.instance().mockAddHousesToStore).toHaveBeenCalled()
+    })
+  })
+
+ 
+
 })
